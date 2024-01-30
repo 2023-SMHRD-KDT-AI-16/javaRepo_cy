922f4cc56ecfe89532ce794746160dcbcb9952cb
User <User@192.168.219.119>
2024-01-30 17:25:25 +0900

    손채영 - 프로젝트랑 MVC 패키지만 생성해두었습니다 파이팅~!

---
+++ guessMovie/src/controller/LoginCon.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class LoginCon {
+// 컨트롤러부분
+}
---
+++ guessMovie/src/view/MovieMain.java
@@ -0,0 +1,9 @@
+package view;
+
+public class MovieMain {
+
+	public static void main(String[] args) {
+		System.out.println("메인입니다");
+	}
+
+}
