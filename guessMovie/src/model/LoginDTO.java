8ad1d244fa960107bc6f34352fbb7cf0cef91a3e
User <User@192.168.219.119>
2024-01-30 17:22:01 +0900

    틀

---
+++ guessMovie/src/model/LoginDTO.java
@@ -0,0 +1,43 @@
+package model;
+
+public class LoginDTO {
+	
+	private String id; // 회원 id
+	private String pw; // 회원 비밀번호
+	private String email; // 회원 이메일 
+	private String name; // 회원 닉네임
+	
+	
+	public LoginDTO(String id, String pw, String email, String name) {
+		this.id = id;
+		this.pw = pw;
+		this.email = email;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
